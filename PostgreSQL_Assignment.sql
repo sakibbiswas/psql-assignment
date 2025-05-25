88e61063bc2c4b69d13a54fcc2d3dc93e2d1dddf
Sakib Biswas <sakibsakib99880@gmail.com>
2025-05-25 09:43:14 +0600

    Register a new ranger add

---
+++ PostgreSQL_Assignment.sql
@@ -0,0 +1,68 @@
+-- Active: 1747539573897@@127.0.0.1@5432@conservation_db
+-- Rangers Table
+CREATE TABLE rangers (
+  ranger_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  region TEXT NOT NULL
+);
+
+-- Species Table
+CREATE TABLE species (
+  species_id SERIAL PRIMARY KEY,
+  common_name TEXT NOT NULL,
+  scientific_name TEXT NOT NULL,
+  discovery_date DATE NOT NULL,
+  conservation_status TEXT DEFAULT 'Unknown'
+);
+
+-- Sightings Table
+CREATE TABLE sightings (
+  sighting_id SERIAL PRIMARY KEY,
+  ranger_id INT REFERENCES rangers(ranger_id),
+  species_id INT REFERENCES species(species_id),
+  sighting_time TIMESTAMP NOT NULL,
+  location TEXT NOT NULL,
+  notes TEXT
+);
+
+-- Insert Rangers
+INSERT INTO rangers (name, region) VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
+
+-- Insert Species
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+-- Insert Sightings
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+SELECT * FROM rangers;
+SELECT * FROM species;
+SELECT * FROM sightings;
+
+--Problem 1
+INSERT INTO rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
+
+--Problem 2
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
+
+--Problem 3
+SELECT * FROM sightings
+WHERE location ILIKE '%Pass%';
+
+--Problem 4
+
+
+
+
